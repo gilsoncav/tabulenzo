8588af83155dbe652400aa459ecb95f3249a688e
Gilson Cavalcanti <gilsoncav@gmail.com>
2022-02-15 18:40:34 -0300

    Initial Commit

---
+++ Tabulenzo/TabulenzoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabulenzoApp.swift
+//  Tabulenzo
+//
+//  Created by Gilson Cavalcanti on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TabulenzoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
